fe0427392c250fc7bb310f23eb1c2ad0eb3d1ec5
André <110039135+AndreKaled@users.noreply.github.com>
2023-09-21 19:26:25 +0000

    adicionando funcionalidade de adicionar e retornar os dados salvos

--- js/main.js
+++ js/main.js
@@ -1,31 +1,47 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem("itens"));
+
+//atualizando itens do localstorage até o html
+itens.forEach((elemento) =>{
+    criarElemento(elemento)
+});
 
 form.addEventListener("submit", (evento) => {
     //sobreescrevendo evento padrao de enviar pra propria pag.
     evento.preventDefault();
 
-    //testando
-    console.log(evento);
     const nome = evento.target.elements["nome"].value;
     const quant = evento.target.elements["quantidade"].value;
 
-    criarElemento(nome,quant);
+    const itemAtual = {
+        "nome": nome,
+        "quant": quant
+    }
+
+    criarElemento(itemAtual);
+
+    //armazenando dados no localstorage do navegador
+
+    //adicionando na lista
+    itens.push(itemAtual);
+
+    //adicionando lista no localstorage, com chave e valor
+    localStorage.setItem("itens", JSON.stringify(itens));
 });
 
 //criando e adicionando elemento com os dados na lista
-function criarElemento(nome, quant){
-    console.log(nome +"|" +quant);
-
+function criarElemento(elemento){
     const novoItem = document.createElement('li');
 
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quant;
+    numeroItem.innerHTML = elemento.quant;
 
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.innerHTML += elemento.nome;
 
     lista.appendChild(novoItem);
 }
+console.log("AAA");
